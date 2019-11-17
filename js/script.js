f5fe142c6b1f21506fbb1a4549d1ba324cd1788b
Shailesh Jain <jainshailesh91@gmail.com>
2019-11-17 14:25:16 +0530

    added social sites

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
 $(function () {
-    /**
-     *
-     */
-    var text = $('.type-erase-animation').data('text');
-    var i = 0;
-    var typeEarse = true;
-    setInterval(function(){
-        // $('.type-erase-animation').append(text.charAt(i++));
-        var t = $(".type-erase-animation").text();
-        if(typeEarse){
-            $('.type-erase-animation').text(t + text.charAt(i++));
-            if(i >= text.length)
-                typeEarse = false;
-        }else{
-            $('.type-erase-animation').text(t.slice(0, i--));
-            if(i < 0)
-                typeEarse = true;
-        }
-        // alert(i);
-    }, 150);
+
+    // function wait(ms)
+    // {
+    //     var d = new Date();
+    //     var d2 = null;
+    //     do { d2 = new Date(); }
+    //     while(d2-d < ms);
+    // }
+    //
+    // var text = $('.type-erase-animation').data('text');
+    // var i = 0;
+    // var typeEarse = true;
+    // setInterval(async function(){
+    //     // $('.type-erase-animation').append(text.charAt(i++));
+    //     var t = $(".type-erase-animation").text();
+    //     if(typeEarse){
+    //         $('.type-erase-animation').text(t + text.charAt(i++));
+    //         if(i >= text.length){
+    //             typeEarse = false;
+    //             $('.blink-underscore').css({
+    //                 marginLeft: '-6%',
+    //             });
+    //         }
+    //     }else{
+    //         $('.type-erase-animation').text(t.slice(0, i--));
+    //         if(i < 0){
+    //             typeEarse = true;
+    //             $('.blink-underscore').css({
+    //                 marginLeft: '0',
+    //             });
+    //         }
+    //     }
+    //     // alert(i);
+    // }, 150);
 });
